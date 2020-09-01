de8cb346bb5ddce4d0e08d415413d2aefcf27eab
Ariel van Zyl <arielvz271@gmail.com>
2020-09-02 00:18:58 +0200

    sliders done for now

--- js.js
+++ js.js
@@ -1,51 +1,48 @@
-$( document ).ready(function() {
+$(document).ready(function () {
   updateAmounts()
 });
 
 // Amount on change, input event
 $('#amount, #term').on('change input', e => updateAmounts());
 
-function updateAmounts(){
-  // update slider values
+function updateAmounts() {
+  // update display values
   $('#amountDsiplay').html($('#amount').val())
   $('#termDsiplay').html($('#term').val())
+  $('#monthlyInstallment').html(montlyInstalment().toFixed(2))
 
-  // calcualte monthly installment
-
+  console.log()
   // update installment value
+}
+
+function montlyInstalment() {
+  let amount = $('#amount').val()
+  let term = $('#term').val()
+
+  const originationFee = 165.0
+  let initiationFee = ((amount<=1000?amount*0.15:((amount>1000&&amount<10000)?originationFee+(amount-1000)*0.1:(amount>=10000?1050:0)))/amount>0.15?amount*0.15:(amount<=1000?amount*0.15:((amount>1000&&amount<10000)?originationFee+(amount-1000)*0.1:(amount>=10000?1050:0))))*1.15
+  let capitalizedAmount = parseFloat(initiationFee) + parseFloat(amount)
+  const serviceFee = 69.00
+  let insurance = (parseFloat(amount) / 1000) * 4.5
+  const interestRate = 0.020625
+  
+  return PMT(interestRate, term, -(capitalizedAmount)) + insurance + serviceFee
 }
 
 
 
-const rates = {
-  1000: { initiation: 0, insurance: 4.5 },
-  2000: { initiation: 0, insurance: 9.0 },
-  3000: { initiation: 0, insurance: 13.5 },
-  4000: { initiation: 0, insurance: 18.0 },
-  5000: { initiation: 0, insurance: 22.5 },
-  6000: { initiation: 0, insurance: 27.0 },
-  7000: { initiation: 0, insurance: 31.5 },
-  8000: { initiation: 0, insurance: 36.0 },
-  9000: { initiation: 0, insurance: 40.5 },
-  10000: { initiation: 0, insurance: 45.0 },
-  11000: { initiation: 0, insurance: 49.5 },
-  12000: { initiation: 0, insurance: 54.0 },
-  13000: { initiation: 0, insurance: 58.5 },
-  14000: { initiation: 0, insurance: 36.1 },
-  15000: { initiation: 0, insurance: 67.5 },
-  16000: { initiation: 0, insurance: 72.0 },
-  17000: { initiation: 0, insurance: 76.5 },
-  18000: { initiation: 0, insurance: 81.0 },
-  19000: { initiation: 0, insurance: 85.5 },
-  20000: { initiation: 0, insurance: 90.0 },
-  21000: { initiation: 0, insurance: 94.5 },
-  22000: { initiation: 0, insurance: 99.0 },
-  23000: { initiation: 0, insurance: 103.5 },
-  24000: { initiation: 0, insurance: 108.0 },
-  25000: { initiation: 0, insurance: 112.5 },
-  26000: { initiation: 0, insurance: 117.0 },
-  27000: { initiation: 0, insurance: 121.5 },
-  28000: { initiation: 0, insurance: 126.0 },
-  29000: { initiation: 0, insurance: 130.5 },
-  30000: { initiation: 0, insurance: 135.0 },
+function PMT(rate, nper, pv, fv, type) {
+  if (!fv) fv = 0;
+  if (!type) type = 0;
+
+  if (rate == 0) return -(pv + fv)/nper;
+  
+  var pvif = Math.pow(1 + rate, nper);
+  var pmt = rate / (pvif - 1) * -(pv * pvif + fv);
+
+  if (type == 1) {
+    pmt /= (1 + rate);
+  };
+
+  return pmt;
 }
